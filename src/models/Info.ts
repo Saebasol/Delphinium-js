5ec44d63b83456706eebb876c8e38e31903a557d
Ryu Juheon <saidbysolo@gmail.com>
2025-06-15 00:48:35 +0900

    BREAKING CHANGE: change info field name
    Signed-off-by: Ryu Juheon <saidbysolo@gmail.com>

--- src/models/Info.ts
+++ src/models/Info.ts
@@ -2,13 +2,13 @@ export interface RawInfoData {
     id: number;
     title: string;
     thumbnail: string;
-    artist: string[];
-    group: string[];
+    artists: string[];
+    groups: string[];
     type: string;
     language: string | null;
     series: string[];
-    character: string[];
-    tag: string[];
+    characters: string[];
+    tags: string[];
     date: string;
 }
 
@@ -16,26 +16,26 @@ export class Info {
     public readonly id: number;
     public readonly title: string;
     public readonly thumbnail: string;
-    public readonly artist: string[];
-    public readonly group: string[];
+    public readonly artists: string[];
+    public readonly groups: string[];
     public readonly type: string;
     public readonly language: string | null;
     public readonly series: string[];
-    public readonly character: string[];
-    public readonly tag: string[];
+    public readonly characters: string[];
+    public readonly tags: string[];
     public readonly date: Date;
 
     constructor(data: RawInfoData) {
         this.id = data.id;
         this.title = data.title;
         this.thumbnail = data.thumbnail;
-        this.artist = data.artist;
-        this.group = data.group;
+        this.artists = data.artists;
+        this.groups = data.groups;
         this.type = data.type;
         this.language = data.language ?? null;
         this.series = data.series; 
-        this.character = data.character;
-        this.tag = data.tag;
+        this.characters = data.characters;
+        this.tags = data.tags;
         this.date = new Date(data.date);
     }
 }
