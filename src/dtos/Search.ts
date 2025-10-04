0f2161a0fa49f1bd67f1e00666f3a88c4f20a5bb
Ryu Juheon <saidbysolo@gmail.com>
2025-10-05 01:07:40 +0900

    fix: heliotrope api changed

--- src/dtos/Search.ts
+++ src/dtos/Search.ts
@@ -1,16 +1,16 @@
 import { Info, RawInfoData } from "../entities";
 
 export interface RawSearchResultData {
-  result: RawInfoData[];
+  results: RawInfoData[];
   count: number;
 }
 
 export class SearchResultDTO {
-  public readonly result: Info[];
+  public readonly results: Info[];
   public readonly count: number;
 
   constructor(data: RawSearchResultData) {
-    this.result = data.result.map(info => new Info(info));
+    this.results = data.results.map(info => new Info(info));
     this.count = data.count;
   }
 }
