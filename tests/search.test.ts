a733b413afdf9b05ba6559c2b1bde16536f24aa8
Ryu Juheon <saidbysolo@gmail.com>
2025-10-05 08:51:59 +0900

    fix: heliotrope api changed (#7)

--- tests/search.test.ts
+++ tests/search.test.ts
@@ -5,7 +5,7 @@ describe('Search', () => {
   test('performs search correctly', async () => {
     const searchResult = await global.client.hitomi.postSearch({ query: ["sekigahara", "artist:tsukako"], offset: 1 });
 
-    expect(searchResult.result).toEqual(expect.any(Array));
+    expect(searchResult.results).toEqual(expect.any(Array));
     expect(searchResult.count).toEqual(expect.any(Number));
   });
 });
